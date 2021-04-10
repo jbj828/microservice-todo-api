4a7e45e61afac5befe4249b315fbcb5f6c2d5c9f
Chung Byung Jae <jbj828@naver.com>
2021-04-11 00:23:08 +0900

    feat: added todo schema

--- src/data-access/index.js
+++ src/data-access/index.js
@@ -3,13 +3,22 @@ const mongoose = require('mongoose');
 const url = process.env.TODOS_DB_URL;
 const dbName = process.env.TODOS_DB_NAME;
 
-mongoose.connect(`${url}${dbName}`, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+async function makeDb() {
+  if (mongoose.connection.readyState === 0) {
+    await mongoose.connect(`${url}${dbName}`, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+  }
 
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', () => {
-  console.log('==> MongoDB Connected!');
-});
+  const db = mongoose.connection;
+  db.on('error', console.error.bind(console, 'connection error:'));
+  db.once('open', () => {
+    console.log('==> MongoDB Connected!');
+  });
+  return db;
+}
+
+module.exports = {
+  makeDb,
+};
