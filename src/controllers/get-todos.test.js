1ca40fe259721d0020766d4cc9c49fc1f997ca8f
Chung Byung Jae <jbj828@naver.com>
2021-04-12 18:49:28 +0900

    test: added 3A pattern comment

--- src/controllers/get-todos.test.js
+++ src/controllers/get-todos.test.js
@@ -4,17 +4,27 @@ const { makeGetTodos } = require('./get-todos');
 
 describe('get todos controller', () => {
   it('successfully get todos list', async () => {
+    // Arrange
     const getTodos = await makeGetTodos({ listTodos: (c) => c });
+
+    // Act
     const res = await getTodos();
+
+    // Assert
     expect(res.statusCode).toEqual(200);
   });
   it('reports get todos errors', async () => {
+    // Arrange
     const getTodos = await makeGetTodos({
       listTodos: () => {
         throw Error('hi error');
       },
     });
+
+    // Act
     const res = await getTodos();
+
+    // Assert
     expect(res.statusCode).toEqual(400);
     expect(res.body.error).toEqual('hi error');
   });
